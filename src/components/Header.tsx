b46d55841317080292e8a3b1daf8f4be5e4f8bac
[dyad] <almeidalima.jvictor@gmail.com>
2025-12-12 18:28:54 -0400

    [dyad] Fixing TypeScript errors in Header component - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,22 @@
-<Link to="/" className="flex items-center space-x-3">
-            <img 
-              src="/images/img-Logo-IEADMS.png" 
-              alt="IEADMS Logo"
-              className="h-10 w-auto object-contain"
-            />
-            <span className="text-white text-xl font-bold">IEADMS</span>
-          </Link>
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-900 text-white py-4 px-6 shadow-lg">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link to="/" className="flex items-center space-x-3">
+          <img 
+            src="/images/img-Logo-IEADMS.png" 
+            alt="IEADMS Logo"
+            className="h-10 w-auto object-contain"
+          />
+          <span className="text-white text-xl font-bold">IEADMS</span>
+        </Link>
+        
+        {/* Navigation menu can be added here */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
