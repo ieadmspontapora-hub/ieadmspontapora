54f96c53da50b99f41dd61d43594a7b3c3e3f530
ieadmspontapora-hub <ieadmspontapora@gmail.com>
2025-12-16 18:09:32 -0400

    Correção das imagens

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
   ];
 
   const carouselImagesMobile = [
-    'https://res.cloudinary.com/dapypkcbb/image/upload/v1765835831/Aqui_Jesus_upscayl_5x_upscayl-standard-4x_vgqoic.png',
+    'https://res.cloudinary.com/dapypkcbb/image/upload/v1765922318/Aqui_Jesus_1_afsrmw.png',
     'https://res.cloudinary.com/dapypkcbb/image/upload/v1765835587/Peniel_upscayl_5x_upscayl-standard-4x_jvp23g.png',
     'https://res.cloudinary.com/dapypkcbb/image/upload/v1765836155/UMADEMATS_upscayl_5x_upscayl-standard-4x_jibdkd.png',
     'https://res.cloudinary.com/dapypkcbb/image/upload/v1765836342/CIFAD_sta85v.png',
